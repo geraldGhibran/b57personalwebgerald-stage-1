d0120b784336f978c990b9c4a858bb2b30101adc
Gerald Ghibran <gerald.gunasha@gmail.com>
2024-09-17 08:59:54 +0700

    fixing vercel dbconfig for neon

--- config/config.js
+++ config/config.js
@@ -1,15 +1,9 @@
 export const dbConfig = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     dialectModule: pg, // I've added this.
     timezone: process.env.TZ,
-    define: {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_general_ci',
-    },
-    logging: false,
   };
